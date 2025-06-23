e140c72bfeaa9a220b54d1d87e68a5e42817037f
teklu abayneh <tekluabayneh@gmail.com>
2025-06-23 20:06:25 +0300

    Add utility functions

---
+++ src/app/utils/server-utils.ts
@@ -0,0 +1,6 @@
+// import "server-only";
+const ServersideFunctions = () => {
+    console.log('This is a placeholder for server-side functions. Implement your server logic here.')
+    return "server-side functions result";
+};
+export default ServersideFunctions;
