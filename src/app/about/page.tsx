5a9f3e61d016dc5c70ab88732c561bf8a93b1223
teklu abayneh <tekluabayneh@gmail.com>
2025-06-23 20:06:24 +0300

    Add about page components

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+const AboutPage = () => {
+    console.log('AboutPage server component rendered')
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>This is the about page of our application.</p>
+        </div>
+    );
+}
+export default AboutPage;
+
