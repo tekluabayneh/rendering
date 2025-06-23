82d795ae3e5c9e970f100b3f998233ed5a27c150
teklu abayneh <tekluabayneh@gmail.com>
2025-06-23 23:40:41 +0300

    Update client-route page

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -1,13 +1,14 @@
 "use client"
 
 import ServerSideFunction from "../utils/server-utils"
-
+import { useTheme } from "../component/theme-provider";
 const clientroutePage = () => {
     const result = ServerSideFunction();
+	const theme = useTheme()
     return (
-        <div>
+        <div style={{background: theme.colors.primary}}>
             <h1>Client Route Page</h1>
-            <p>This page is rendered on the client side.</p>
+            <p style={{color: theme.colors.seconday}}>This page is rendered on the client side.</p>
             <p>Server-side function result: {result}</p>
         </div>
     );
